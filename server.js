df4598b859316cd1c4f568ccb42828eb53b1dd2c
PRANESH-MANICKAM <95580533+PRANESH-MANICKAM@users.noreply.github.com>
2025-04-22 09:54:07 +0000

    Removed default port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT;
 
 app.get("/health", (req, res) => {
     res.json({
